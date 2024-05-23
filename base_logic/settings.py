9b43820474bb93e8608e860ee0b108a3e389479d
Anastasiya Begunova <123498835+Egessihora@users.noreply.github.com>
2024-05-23 21:46:09 +0300

    Update settings.py

--- base_logic/settings.py
+++ base_logic/settings.py
@@ -1,4 +1,1 @@
-base_url = 'https://xxx.xxx.xxx.xxx'  # IPv4 Веб-интерефейса TLS Gateway
-admin_port = 'xxx'                  # Порт для доступа администратора
-user_twoside_port = 'xxx'           # Порт для доступа пользователей с двусторонней аутентификацией
-user_oneside_port = 'xxx'          # Порт для доступа пользователей с односторонней аутентификацией
+base_url = 'https://xxx.xxx.xxx.xxx'  # Базовый URL веб-приложения
