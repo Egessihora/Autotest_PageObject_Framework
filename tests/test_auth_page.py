16ff703090586804a4906ad9ce825d6a229e0d87
Anastasiya Begunova <123498835+Egessihora@users.noreply.github.com>
2024-05-25 18:27:08 +0300

    Update test_auth_page.py

--- tests/test_auth_page.py
+++ tests/test_auth_page.py
@@ -5,6 +5,6 @@ from base_logic.pages.Auth_page_APP import *
 
 # TEST-XXX
 def test_auth_page_open(browser):
-    page = AuthPage(browser)
     print(f"\nTEST-XXX \nПроверка успешного открытия главной страницы")
+    page = AuthPage(browser)
     assert page.get_relative_link() == "/"
