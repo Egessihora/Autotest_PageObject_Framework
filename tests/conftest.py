0c3cbf6c4324b280ad019baa54f003bac0546dcb
Anastasiya Begunova <123498835+Egessihora@users.noreply.github.com>
2024-05-25 20:03:10 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,13 +9,5 @@ def browser():
     driver = webdriver.Chrome()  # инициализируем браузер
     driver.maximize_window()
     yield driver
-    #
-    # if request.node.rep_call.failed:
-    #     # Make the screen-shot if test failed:
-    #     try:
-    #         driver.screenshot('screenshots/' + str() + '.png')
-    #
-    # except:
-    #         pass
     print("\nquit browser")
     driver.quit()  # закрываем браузер
